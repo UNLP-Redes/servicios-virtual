ab608e5a245bf2c8c617cc89993e9f7ecdcc2fee
Matías Emanuel Ferrigno <meferrigno@gmail.com>
2022-04-01 16:00:37 -0300

    fix ya no es entregable

--- http/src/restringido/the-end.php
+++ http/src/restringido/the-end.php
@@ -2,7 +2,6 @@
 $date = date('Y-m-d H:i:s');
 $string = $date . "redesycom";
 echo "¡Felicitaciones, llegaste al final del ejercicio! \n
-En la entrega adjuntá la siguiente información: \n
 Fecha: " . $date . "
 Verificación: " . hash("sha256", $string);
 ?>
